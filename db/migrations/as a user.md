344b073d3d0ea4253e96fe38017609d28d350204
ibrahim pala <ibrahimpala2019@gmail.com>
2022-06-08 14:21:20 +0100

    bookmark manager

---
+++ db/migrations/as a user.md
@@ -0,0 +1,16 @@
+as a user 
+i want to see a list of bookmarks
+so that 
+
+As a time-pressed user
+So that I can quickly go to web sites I regularly visit
+I would like to see a list of bookmarks
+
+They’re usually written in the format:
+
+As a… [who is the user?]
+
+I need/want/expect to… [what does the user want to do?]
+
+So that… [why does the user want to do this?]
+
---
+++ spec/features/viewing_bookmarks_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# As a time-pressed user
+# So that I can quickly go to web sites I regularly visit
+# I would like to see a list of bookmarks
+
+feature 'Viewing bookmarks' do
+  scenario 'A user can see bookmarks' do
+    visit('/bookmarks')
+
+    expect(page).to have_content 'http://www.makersacademy.com'
+    expect(page).to have_content 'http://www.destroyallsoftware.com'
+    expect(page).to have_content 'http://www.google.com'
+  end
+end
